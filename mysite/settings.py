dd97106759089d441c4691612894a9d7c205627a
eomur <eomur@DESKTOP-73L7CKH>
2026-06-15 21:55:40 +0300

    Deploy fit service site

---
+++ mysite/settings.py
@@ -0,0 +1,69 @@
+import os
+import dj_database_url
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,.onrender.com'
+).split(',')
+
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'https://*.onrender.com'
+).split(',')
+
+INSTALLED_APPS=[
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+'app',
+]
+
+MIDDLEWARE=[
+'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware',
+'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ROOT_URLCONF='mysite.urls'
+
+TEMPLATES=[{
+'BACKEND':'django.template.backends.django.DjangoTemplates',
+'APP_DIRS':True,
+'OPTIONS':{
+'context_processors':[
+'django.template.context_processors.debug',
+'django.template.context_processors.request',
+'django.contrib.auth.context_processors.auth',
+'django.contrib.messages.context_processors.messages',
+],
+},
+}]
+
+WSGI_APPLICATION='mysite.wsgi.application'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
+}
+
+LANGUAGE_CODE='ru-ru'
+TIME_ZONE='UTC'
+STATIC_URL='/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+
+LOGIN_URL = '/cabinet/'
